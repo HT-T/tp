44310f104ef6d3b6c199be18981dd16664b2bcbc
HT-T <kikutahang@gmail.com>
2022-10-24 13:14:02 +0800

    no message

---
+++ src/main/java/seedu/duke/commands/CommandPlanTimetable.java
@@ -0,0 +1,23 @@
+package seedu.duke.commands;
+
+import seedu.duke.Exceptions;
+import seedu.duke.Timetable;
+import seedu.duke.UI;
+import seedu.duke.module.Module;
+import seedu.duke.module.lessons.Lesson;
+//import seedu.duke.data.AttendingManager;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+import java.util.List;
+import java.util.Objects;
+
+public class CommandPlanTimetable { /////////// REMOVE import WHEN DONE
+
+    private static final Logger lgr = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
+    List<Module> lessons = Timetable.getListOfModules();
+
+
+
+}
